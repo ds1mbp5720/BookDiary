99f3569f54456f77c06043634d02bd975055e26d
ds1mbp5720 <ds1mbp5720@naver.com>
2023-03-02 22:21:11 +0900

    Search Fragment 및 adapter구성요소 추가

--- app/src/main/java/com/lee/bookdiary/search/SearchFragment.kt
+++ app/src/main/java/com/lee/bookdiary/search/SearchFragment.kt
@@ -1,13 +1,16 @@
 package com.lee.bookdiary.search
 
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.lee.bookdiary.R
 import com.lee.bookdiary.base.BaseFragment
 import com.lee.bookdiary.databinding.SearchFragmentBinding
+import com.lee.bookdiary.search.adapter.SearchAdapter
 
 class SearchFragment:BaseFragment<SearchFragmentBinding,SearchViewModel>(){
     override val layoutId = R.layout.search_fragment
     override val viewModel: SearchViewModel by viewModels()
+    private lateinit var searchAdapter: SearchAdapter
 
     companion object {
         const val TAG = "SearchFragment"
@@ -19,5 +22,10 @@ class SearchFragment:BaseFragment<SearchFragmentBinding,SearchViewModel>(){
 
     override fun initObserve() {
         super.initObserve()
+
+        viewModel.deleteClick.observe(this){
+            dataBinding.etSearch.setText("")
+            dataBinding.recyclerVwBookInfo.isVisible = false
+        }
     }
 }
--- app/src/main/java/com/lee/bookdiary/search/SearchViewModel.kt
+++ app/src/main/java/com/lee/bookdiary/search/SearchViewModel.kt
@@ -1,8 +1,16 @@
 package com.lee.bookdiary.search
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import com.lee.bookdiary.base.BaseViewModel
+import com.lee.bookdiary.base.SingleLiveEvent
 
 class SearchViewModel(application: Application): BaseViewModel(application) {
+    private val _deleteClick = SingleLiveEvent<Unit>()
+    val deleteClick: LiveData<Unit> get() =  _deleteClick
+
+    fun onDeleteCLick() {
+        _deleteClick.call()
+    }
 
 }
